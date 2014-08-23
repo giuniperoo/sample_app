e7df6c37cf21affad441fbe3a2fe77afa939d7ce
Troy Giunipero <giunipero@gmail.com>
2014-08-23 23:46:18 +0200

    add contact page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -44,4 +44,16 @@ feature 'StaticPages', type: :feature do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+  describe 'Contact page' do
+    it 'should have the content "Contact"' do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it 'should have the title "Contact"' do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+    end
+  end
 end
