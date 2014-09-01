4e8b4b1a0f3289ee553b4c92ab27d425e4ff4d0e
Troy Giunipero <giunipero@gmail.com>
2014-09-01 18:49:05 +0200

    finish user signup

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,13 +1,34 @@
 require 'rails_helper'
 
-feature 'User', type: :feature do
+feature 'User pages', type: :feature do
   subject { page }
 
   describe 'signup page' do
+    let(:submit) { 'Create my account' }
+
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title('| Sign up') }
+
+    describe 'with invalid information' do
+      it 'should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with valid information' do
+      before do
+        fill_in 'Name',         with: 'Example User'
+        fill_in 'Email',        with: 'user@example.com'
+        fill_in 'Password',     with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
+      end
+
+      it 'should create a user' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
   describe 'profile page' do
