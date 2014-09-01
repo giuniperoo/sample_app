35544a931c0d3865c1a0fc73a26534359865dbe0
Troy Giunipero <giunipero@gmail.com>
2014-09-01 22:35:58 +0200

    misc. improvements

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -15,6 +15,13 @@ feature 'User pages', type: :feature do
       it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe 'after submission' do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe 'with valid information' do
@@ -27,6 +34,14 @@ feature 'User pages', type: :feature do
 
       it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
