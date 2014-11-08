810ecf2ac36aa008f6ef5d3b82253d55ada39fde
Troy Giunipero <giunipero@gmail.com>
2014-11-08 19:52:24 +0100

    fine-tune authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,8 +39,24 @@ module SessionsHelper
     end
   end
 
+  # returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+
   # returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
+  end
+
+  # redirects to stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   # returns the hash digest of the given string
   def User.digest(string)
